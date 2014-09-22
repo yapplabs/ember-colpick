4c9833fa3591e0476661461ab032ba45dc2fae35
Stefan Penner <stefan.penner@gmail.com>
2014-09-22 15:40:47 -0400

    fix generator

--- blueprints/ember-colpick/index.js
+++ blueprints/ember-colpick/index.js
@@ -1,4 +1,10 @@
 module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject('colpick', '2.0.2');
   } 
