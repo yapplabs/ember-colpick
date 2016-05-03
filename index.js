cc26880b1fe44c75093838ab1ef508de455f062a
Georg Kothmeier <georg.kothmeier@roomle.com>
2016-05-03 14:53:03 +0200

    Make fastboot guard

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ module.exports = {
   },
   included: function colpick_included(app) {
     this._super.included.apply(this, arguments);
+    if(!process.env.EMBER_CLI_FASTBOOT) {
+      var colpickPath = path.join(app.bowerDirectory, 'colpick');
 
-    var colpickPath = path.join(app.bowerDirectory, 'colpick');
-
-    this.app.import(path.join(colpickPath, 'js',  'colpick.js'));
-    this.app.import(path.join(colpickPath, 'css', 'colpick.css'));
+      this.app.import(path.join(colpickPath, 'js',  'colpick.js'));
+      this.app.import(path.join(colpickPath, 'css', 'colpick.css'));
+    }
   }
 };
