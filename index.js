f720c0f6342501a3c67d954fdec3f8aa44d38652
Bill Dami <bdami@gavant.com>
2017-09-20 10:32:16 -0400

    add compatibility for FastBoot >=1.0.0

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 'use strict';
 var path = require('path');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-colpick',
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
+  treeForVendor(defaultTree) {
+    var trees = [];
+
+    if (defaultTree) {
+      trees.push(defaultTree);
+    }
+
+    var browserVendorLib = new Funnel(path.join(this.app.bowerDirectory, 'colpick', 'js'), {
+      files: ['colpick.js'],
+      destDir: 'colpick'
+    });
+
+    browserVendorLib = map(browserVendorLib, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+    trees.push(browserVendorLib);
+    return new mergeTrees(trees);
+  },
   included: function colpick_included(app) {
     this._super.included.apply(this, arguments);
-    if(!process.env.EMBER_CLI_FASTBOOT) {
-      var colpickPath = path.join(app.bowerDirectory, 'colpick');
-
-      this.app.import(path.join(colpickPath, 'js',  'colpick.js'));
-      this.app.import(path.join(colpickPath, 'css', 'colpick.css'));
-    }
+    this.app.import(path.join('vendor', 'colpick',  'colpick.js'));
+    this.app.import(path.join(app.bowerDirectory, 'colpick', 'css', 'colpick.css'));
   }
 };
