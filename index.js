05c37052154c58404074209cd36052c1e4dcc8ea
Pat O'Callaghan <patocallaghan@gmail.com>
2018-02-07 23:27:58 +0000

    Remove colpick Bower package with npm one

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ module.exports = {
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
-  included: function colpick_included(app) {
+  included: function colpick_included() {
     this._super.included.apply(this, arguments);
     if(!process.env.EMBER_CLI_FASTBOOT) {
-      var colpickPath = path.join(app.bowerDirectory, 'colpick');
+      var colpickPath = path.join('node_modules', 'jquery-colpick');
 
       this.app.import(path.join(colpickPath, 'js',  'colpick.js'));
       this.app.import(path.join(colpickPath, 'css', 'colpick.css'));
