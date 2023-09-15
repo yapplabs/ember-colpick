906382fb0c06f7799400b99fcf66467781b98ac0
Luke Melia <luke@lukemelia.com>
2023-09-15 00:01:30 -0400

    Use Github Actions

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,60 +1,75 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
+    useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.16',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0',
+            'ember-source': '~3.24.3',
+            'ember-template-imports': null,
+          },
+        },
+        allowedToFail: true, // to fix this, we need some build-time logic to avoid trying to render gts component
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+            '@ember/test-helpers': '2.8.1',
+            'ember-qunit': '5.1.5',
+            '@types/ember__test-helpers': '^2.8.1',
+            '@types/ember-qunit': '^5.0.2',
           },
         },
       },
       {
-        name: 'ember-lts-3.20',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.5',
+            'ember-source': '~4.4.0',
           },
         },
       },
       {
         name: 'ember-release',
         npm: {
+          dependencies: {
+            'ember-auto-import': '^2.0.0',
+          },
           devDependencies: {
             'ember-source': await getChannelURL('release'),
+            webpack: '^5.0.0',
           },
         },
       },
       {
         name: 'ember-beta',
         npm: {
+          dependencies: {
+            'ember-auto-import': '^2.0.0',
+          },
           devDependencies: {
             'ember-source': await getChannelURL('beta'),
+            webpack: '^5.0.0',
           },
         },
       },
       {
         name: 'ember-canary',
         npm: {
+          dependencies: {
+            'ember-auto-import': '^2.0.0',
+          },
           devDependencies: {
             'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
+            webpack: '^5.0.0',
           },
         },
       },
@@ -68,11 +83,20 @@ module.exports = async function () {
           }),
         },
         npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+            '@ember/test-helpers': '2.8.1',
+            'ember-qunit': '5.1.5',
+            '@types/ember__test-helpers': '^2.8.1',
+            '@types/ember-qunit': '^5.0.2',
+          },
           ember: {
             edition: 'classic',
           },
         },
       },
+      embroiderSafe(),
+      embroiderOptimized(),
     ],
   };
 };
